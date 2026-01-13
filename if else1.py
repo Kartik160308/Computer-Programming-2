e0b30e136fa92cbb1abbd90d651537afc3b42df1
Kartik <kartik.n.solanki16@gmail.com>
2026-01-13 10:04:24 +0530

    Add files via upload

---
+++ if else1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+
+if a>b:
+    print("largest number", a)
+    print("smallest number", b)
+
+else:
+    print("Smaller number", a)
+    print("Largest number", b)
